2de25bae86d01483024febfa1e1202b1b1956848
Kelvin Abaidoo <kabaidoo@hotmail.com>
2022-03-16 21:05:13 +0000

    created background gradient

---
+++ app/src/main/java/com/example/todo/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.todo;
+
+import static androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_YES;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_YES);
+    }
+}
