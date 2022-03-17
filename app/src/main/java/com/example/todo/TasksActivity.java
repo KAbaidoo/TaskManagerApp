4854bcddfaa2f0c2f92664f836a54d08b449521d
Kelvin Abaidoo <kabaidoo@hotmail.com>
2022-03-17 22:50:12 +0000

    implemented fab, navDrawer & menuItem

--- app/src/main/java/com/example/todo/TasksActivity.java
+++ app/src/main/java/com/example/todo/TasksActivity.java
@@ -1,14 +1,58 @@
 package com.example.todo;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.bottomappbar.BottomAppBar;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.snackbar.Snackbar;
 
 public class TasksActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tasks);
+
+//        handle fab
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                displayToast("fab clicked");
+            }
+        });
+
+//handle navigation drawer
+        BottomAppBar bottomAppBar = findViewById(R.id.bottomAppBar);
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = (NavigationView)
+                findViewById(R.id.design_navigation_view);
+        bottomAppBar.setNavigationOnClickListener(v -> drawer.openDrawer(navigationView));
+
+//        handle bottomAppBar menu
+        bottomAppBar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                displayToast("Menu item clicked");
+                return true;
+            }
+        });
     }
+
+    private void displayToast(String string) {
+        Toast.makeText(this, string, Toast.LENGTH_SHORT).show();
+    }
+
+
 }
