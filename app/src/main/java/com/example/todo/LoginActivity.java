bf2682bc1c22e774d989ca3cc5391086504abede
Kelvin Abaidoo <kabaidoo@hotmail.com>
2022-03-17 07:28:44 +0000

    added navigation drawer

--- app/src/main/java/com/example/todo/LoginActivity.java
+++ app/src/main/java/com/example/todo/LoginActivity.java
@@ -5,15 +5,27 @@ import static androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_YES;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
 
+//        set the theme to default to dark mode
         AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_YES);
+        startTaskActivity();
+
     }
+
+    public void startTaskActivity() {
+        Intent intent = new Intent(this, TasksActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
 }
